a29158e8f8dec155a8a033d75f0091a2e576d03e
Max Dayton <daytonmaximus@gmail.com>
2020-03-05 14:24:40 -0700

    fixed sidedrawer, implemented backdrop and dismissal

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,15 +2,24 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxilliary';
 
 const sideDrawer = (props) => {
+    let attatchedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attatchedClasses = [classes.SideDrawer, classes.Open];
+    }
     return (
-        <div className={classes.SideDrawer}>
-            <Logo />
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attatchedClasses.join(' ')}>
+                <Logo height="11%" marginBottom="32px"/>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
     );
 };
 
