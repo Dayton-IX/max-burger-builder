0df2abc7da036ed4233c9b197f9117938b78b06f
Max Dayton <daytonmaximus@gmail.com>
2020-03-10 13:55:44 -0600

    removing interceptors when unmount to prevent memory leaks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-function App() {
-  return (
-    <div >
-      <Layout>
-        <BurgerBuilder />
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  //? TESTING
+  // state ={
+  //   show: true
+  // };
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.setState({show: false});
+  //   }, 5000);
+  // }
+
+  render() {
+    return (
+      <div >
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
