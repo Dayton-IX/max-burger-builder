0864d1a273d2499abf5fc9aea42f68e9d69223f7
Max Dayton <daytonmaximus@gmail.com>
2020-03-25 14:06:40 -0600

    form invalid feedback and styling

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,16 +3,22 @@ import classes from './Input.module.css';
 
 const Input = (props) => {
     let inputElement = null;
+    const inputClasses = [classes.InputElement];
+
+    if (props.invalid && props.elementType !== 'select') {
+        inputClasses.push(classes.Invalid);
+    }
+
     switch (props.elementType) {
         case('input'): 
-            inputElement = <input onChange={props.changed} className={classes.InputElement} {...props.elementConfig} value={props.value} />;
+            inputElement = <input onChange={props.changed} className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} />;
             break;
         case ('textarea'):
-            inputElement = <textarea onChange={props.changed} className={classes.InputElement} {...props.elementConfig} value={props.value} />;
+            inputElement = <textarea onChange={props.changed} className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} />;
             break;
         case ('select'):
             inputElement = (
-                <select onChange={props.changed} className={classes.InputElement} value={props.value}>
+                <select onChange={props.changed} className={inputClasses.join(' ')} value={props.value}>
                     {props.elementConfig.options.map(option => (
                         <option key={option.value} value={option.value}>
                             {option.displayValue}
@@ -22,7 +28,7 @@ const Input = (props) => {
             );
             break;
         default: 
-            inputElement = <input onChange={props.changed} className={classes.InputElement} {...props.elementConfig} value={props.value} />;
+            inputElement = <input onChange={props.changed} className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} />;
     }
 
     return (
